1f0ed76a8aa143b1cd5e71f67243d58b24df05a1
Akshansh Gusain <akshanshgusain@gmail.com>
2023-08-02 09:28:57 +0530

    refactor: fixes on 02 August 2023

--- Heap_002.cpp
+++ Heap_002.cpp
@@ -10,7 +10,7 @@
 
 using namespace std;
 
-void heapify(int arr[], int n, int i) {
+void bottomUpHeapify(int arr[], int n, int i) {
     int largest = i; // Initialize largest as root
     int l = 2 * i + 1; // left = 2*i + 1
     int r = 2 * i + 2; // right = 2*i + 2
@@ -28,7 +28,7 @@ void heapify(int arr[], int n, int i) {
         swap(arr[i], arr[largest]);
 
         // Recursively heapify the affected sub-tree
-        heapify(arr, n, largest);
+        bottomUpHeapify(arr, n, largest);
     }
 }
 
@@ -41,7 +41,7 @@ void printArray(int arr[], int n) {
 void heapSort(int arr[], int n) {
     // Build heap (rearrange array)
     for (int i = (n / 2) - 1; i >= 0; i--)
-        heapify(arr, n, i);
+        bottomUpHeapify(arr, n, i);
 
     // One by one extract an element from heap
     for (int i = n - 1; i > 0; i--) {
@@ -49,7 +49,7 @@ void heapSort(int arr[], int n) {
         swap(arr[0], arr[i]);
 
         // call max heapify on the reduced heap
-        heapify(arr, i, 0);
+        bottomUpHeapify(arr, i, 0);
     }
 }
 
