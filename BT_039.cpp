4fae2ea724b8b0362bd3b7a1c6678a2d4849cd99
Akshansh Gusain <akshanshgusain@gmail.com>
2023-08-06 08:30:16 +0530

    feat: add BT_039

---
+++ BT_039.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Akshansh Gusain on 07/08/23.
+//
+
+#include "BT_039.h"
---
+++ BT_039.h
@@ -0,0 +1,14 @@
+//
+// Created by Akshansh Gusain on 07/08/23.
+//
+
+#ifndef FINAL500DSA_BT_039_H
+#define FINAL500DSA_BT_039_H
+
+
+class BT_039 {
+
+};
+
+
+#endif //FINAL500DSA_BT_039_H
