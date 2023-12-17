c0d2e095c9a6a69fcda7a9f06ac1d1da63173f18
Akshansh Gusain <akshanshgusain@gmail.com>
2023-12-17 19:45:49 +0530

    add Kruskals algorithm, completed with DSU

--- Graph/Graph_021.cpp
+++ Graph/Graph_021.cpp
@@ -3,11 +3,13 @@
 //
 
 #include<bits/stdc++.h>
+
 using namespace std;
+
 class Graph {
 public:
     int V, E;
-    vector<vector<pair<int,int>>> adj;
+    vector<vector<pair<int, int>>> adj;
 
     Graph(int V) {
         this->V = V;
@@ -17,14 +19,14 @@ public:
     // Undirected Graph
 
     void addEdgeUWeighted(int u, int v, int w) {
-        adj[v].emplace_back(u,w);
-        adj[u].emplace_back(v,w);
+        adj[v].emplace_back(u, w);
+        adj[u].emplace_back(v, w);
         E++;
     }
 
     // Directed Graph
-    void addEdgeWeighted(int v, int u, int w){
-        adj[v].emplace_back(u,w);
+    void addEdgeWeighted(int v, int u, int w) {
+        adj[v].emplace_back(u, w);
         E++;
     }
 
@@ -34,7 +36,7 @@ public:
         for (const auto &vertexList: this->adj) {
             cout << i << "th - ";
             for (auto it: vertexList) {
-                cout << it.first << " weighted at: "<<it.second<<" ";
+                cout << it.first << " weighted at: " << it.second << " ";
             }
             cout << endl;
             i++;
@@ -42,11 +44,98 @@ public:
     }
 };
 
-void kruskalMST(Graph &g){
+class DisjointSet {
+    vector<int> rank;
+    vector<int> parent;
+
+    void unionByRank(int node1, int node2) {
+        node1 = findParent(node1);
+        node2 = findParent(node2);
+
+        if (node1 == node2) {
+            return;
+        }
+
+        if (rank[node1] > rank[node2]) {
+            parent[node2] = node1;
+        } else if (rank[node2] > rank[node1]) {
+            parent[node1] = node2;
+        } else {
+            parent[node2] = node1;
+            rank[node1]++;
+        }
+    }
+
+    void unionBySize(int node1, int node2) {}
+
+public:
+    // works as make_set function
+    explicit DisjointSet(int nodes) {
+        rank.resize(nodes + 1, 0); // every starting rank is 0
+        parent.resize(nodes + 1);
+        for (int i = 0; i <= nodes; i++) {
+            parent[i] = i; // every node is its own parent
+        }
+    }
+
+    int findParent(int node) {
+        if (node == parent[node]) {
+            return node;
+        }
+        // path compression step
+        return parent[node] = findParent(parent[node]);
+    }
+
+
+    void makeUnion(int node1, int node2, int type = 1) {
+        if (type == 1) {
+            unionByRank(node1, node2);
+        } else if (type == 2) {
+            unionBySize(node1, node2);
+        } else {
+            cout << "invalid type of union1" << endl;
+        }
+    }
+};
+
+void sortEdges(Graph &g, vector<pair<int, pair<int, int>>> &edges){
+    for(int i=0; i<g.V; i++){
+        for(auto it: g.adj[i]){
+            int nextNode = it.first;
+            int weight = it.second;
+            int node = i;
+            edges.push_back({weight, {node, nextNode}});
+        }
+    }
+
+    sort(edges.begin(), edges.end());
+}
+
+void kruskalMST(Graph &g) {
+    vector<pair<int, pair<int, int>>> edges;
+    sortEdges(g, edges);
+
+    DisjointSet ds(g.V);
+    int mstW = 0;
+    vector<pair<int, int>> mst;
+
+    for(auto it: edges){
+        int wt = it.first;
+        int u = it.second.first;
+        int v = it.second.second;
+        if(ds.findParent(u) != ds.findParent(v)){
+            mstW += wt;
+            ds.makeUnion(u, v);
+            mst.push_back({u,v});
+        }
+    }
+
+    cout << mstW << endl;
+    for(auto it : mst) cout << it.first << " - " << it.second << endl;
 
 }
 
-int main(){
+int main() {
     Graph graph(6);
     graph.addEdgeUWeighted(0, 1, 5);
     graph.addEdgeUWeighted(0, 2, 10);
