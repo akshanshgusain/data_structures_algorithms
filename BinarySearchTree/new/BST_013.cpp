08c8cc5d0cf06d9ab808d6db609b990ba799fc11
Akshansh Gusain <akshanshgusain@gmail.com>
2024-03-19 18:46:56 +0530

    bst(fix): k largest element

--- BinarySearchTree/new/BST_013.cpp
+++ BinarySearchTree/new/BST_013.cpp
@@ -4,46 +4,50 @@
 #include "BST_000.cpp"
 
 // constant space: O(1)
-// inorder morris traversal
+// inorder morris traversal - reversed
 void kthLargestElementOptimised(TreeNode* root, int k){
 
     auto currentNode = root;
     int counter = 0;
+    int kthLargest = 0;
 
     while(currentNode != nullptr){
-        // case 1: currentNode has no left tree
-        if(currentNode->left == nullptr){
+        // case 1: currentNode has no right tree
+        if(currentNode->right == nullptr){
             // process current node
             counter++;
             if(counter == k){
-                cout << k << "th Largest number: " << root->val << endl;
+                kthLargest = currentNode->val;
+                break;
             }
-            currentNode = currentNode->right;
+            currentNode = currentNode->left;
         }else{
-            // currentNode has a left subtree
-            // go to the right most TreeNode of the left subtree
-            auto nextNode  = currentNode->left;
-            while(nextNode->right != nullptr and nextNode->right != currentNode){
-                nextNode = nextNode->right;
+            // currentNode has a right subtree
+            // go to the left most TreeNode of the right subtree
+            auto nextNode  = currentNode->right;
+            while(nextNode->left != nullptr and nextNode->left != currentNode){
+                nextNode = nextNode->left;
             }
 
             // case 2: right most child is pointing to nullptr
-            if(nextNode->right == nullptr){
-                nextNode->right = currentNode;
-                currentNode = currentNode->left;
+            if(nextNode->left == nullptr){
+                nextNode->left = currentNode;
+                currentNode = currentNode->right;
             }else{
                 // case 3: right most child is pointing to currentNode
                 // process current node
                 counter++;
                 if(counter == k){
-                    cout << k << "th Largest number: " << root->val << endl;
+                    kthLargest = currentNode->val;
+                    break;
                 }
-                nextNode->right = nullptr;
-                currentNode = currentNode->right;
+                nextNode->left = nullptr;
+                currentNode = currentNode->left;
             }
 
         }
     }
+    cout << k << "th Largest number: " << kthLargest << endl;
 }
 
 void kthLargestElement(TreeNode *root, int &counter, int k) {
@@ -71,8 +75,8 @@ int main() {
     insertInBST(root, 80);
     int counter = 0;
 
-    kthLargestElement(root, counter, 4);
-    kthLargestElementOptimised(root, 4);
+//    kthLargestElement(root, counter, 4);
+    kthLargestElementOptimised(root, 3);
 
     return 0;
 }
